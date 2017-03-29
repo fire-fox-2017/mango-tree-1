79fd9b6c87b6a0243da9193ce548bf214ad87ce3
Bill Ivan <billivank_96@yahoo.co.id>
2017-03-29 19:14:05 +0700

    selesai tapi class mango gak kepake

--- mango_tree_1.js
+++ mango_tree_1.js
@@ -1,19 +1,34 @@
 "use strict"
 
 class MangoTree {
-  
+
   // Initialize a new MangoTree
   constructor() {
+    this._age=0;
+    this._height=0;
+    this._max=20;
+    this._fruit=0;
+    this._healthy=true;
+    this._harvested=0;
+    this._good = 0;
+    this._bad = 0;
   }
 
   getAge() {
+    return this._age;
   }
   getHeight() {
-
+    return this._height;
   }
   getFruits() {
+    return this._fruit;
   }
   getHealtyStatus() {
+    if(this._healthy){
+      return "The tree is alive!";
+    } else {
+      return "The tree has meet its end!";
+    }
   }
 
 
@@ -21,14 +36,30 @@ class MangoTree {
 
   // Grow the tree
   grow() {
+    this._age += 1;
+    if(this._age <= 10){
+      this._height += Math.floor(Math.random()*5)+1;
+    }
+    if(this._age === 20){
+      this._healthy=false;
+    }
   }
 
   // Produce some mangoes
   produceMangoes() {
+    if(this._age <= 20 && this._fruit <= this._max){
+      this._fruit += Math.floor(Math.random()*this._max)+1;
+      this._good = Math.floor(Math.random()*this._fruit)+1;
+      this._bad = (this._fruit-this._good);
+      return this._fruit;
+    }
   }
 
   // Get some fruits
   harvest() {
+    this._harvested=this._fruit;
+    this._fruit=0;
+    return this._harvested;
   }
 
 }
@@ -36,11 +67,23 @@ class MangoTree {
 class Mango {
   // Produce a mango
   constructor() {
+
   }
 }
 
 function getRandomNumber() {
 }
+
+var tree = new MangoTree()
+console.log(`The tree is alive! :smile:`)
+do{
+  tree.grow()
+  tree.produceMangoes()
+  tree.harvest()
+  console.log(`[Year ${tree._age} Report] Height = ${tree._height} m | Fruits harvested = ${tree._harvested} (${tree._good} good, ${tree._bad} bad)`)
+} while (tree._healthy != false)
+
+console.log(`The tree has met its end. :sad:`)
 
 module.exports = {
   MangoTree,
