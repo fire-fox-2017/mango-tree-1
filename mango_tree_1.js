44a0280b0ab65a41386f5b121140d995a7ccb895
arahito <uarahito@yahoo.com>
2017-03-31 19:42:23 +0700

    Mango Tree

--- mango_tree_1.js
+++ mango_tree_1.js
@@ -1,34 +1,76 @@
 "use strict"
 
 class MangoTree {
-  
+
   // Initialize a new MangoTree
   constructor() {
+    this._age = 0
+    this._maxAge = getRandomNumber(7)
+    this._height = 0
+    this._tempFruit = [];
+    this._maxFruit = getRandomNumber(10)
+    this._healthy = true
+    this._harvested = ''
+    this._tempG = 0
+    this._tempB = 0
   }
 
   getAge() {
+    return this._age
   }
+
   getHeight() {
-
+    return this._height
   }
+
   getFruits() {
-  }
-  getHealtyStatus() {
+    return this._maxFruit+' '+this.countFruit+' '+this.pickFruit
   }
 
+  getHealtyStatus() {
+    return this._good+' '+this._bad;
+  }
 
   // Get current states here
 
   // Grow the tree
   grow() {
+    if (this._age <= this._maxAge) {
+      this._age += 1;
+      if (this._age < this._maxAge - 2) {
+        this._height += getRandomNumber(3);
+      }
+
+      if (this._age >= this._maxAge) {
+        this._healthy = false
+      }
+    } else {
+      this._healthy = false
+    }
   }
 
   // Produce some mangoes
   produceMangoes() {
+    for (let i = 0; i < this._maxFruit; i++) {
+      let mango = new Mango()
+      this._tempFruit.push(mango)
+    }
   }
 
   // Get some fruits
   harvest() {
+    for (let i = 0; i < this._tempFruit.length; i++) {
+      if (this._tempFruit[i].quality === 'good') {
+        this._tempG += 1
+      } else {
+        this._tempB += 1
+      }
+      this._harvested = `${this._tempG + this._tempB} (${this._tempG} good, ${this._tempB} bad)`
+    }
+    this._tempFruit = [];
+    this._tempG = 0
+    this._tempB = 0
+
   }
 
 }
@@ -36,11 +78,28 @@ class MangoTree {
 class Mango {
   // Produce a mango
   constructor() {
+    let type = ['good', 'bad'];
+    let randType = Math.floor((Math.random() * type.length)+0);
+    this.quality = type[randType];
   }
 }
 
-function getRandomNumber() {
+function getRandomNumber(num) {
+  return Math.floor(Math.random() * num) + 1;
 }
+
+let tree = new MangoTree();
+console.log(`The tree is alive! :smile"`);
+
+do {
+  tree.grow()
+  tree.produceMangoes()
+  tree.harvest()
+  console.log(`[Year ${tree._age} Report] Height = ${tree._height} | Fruits harvested = ${tree._harvested}`);
+} while (tree._healthy != false);
+console.log(`The tree is alive! :sad"`);
+
+// console.log(getRandomNumber(5));
 
 module.exports = {
   MangoTree,
